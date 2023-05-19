a3f813f983297d1c3d9d5e66df95db1e82e85432
radenlarko <radenlarko@gmail.com>
2023-05-19 16:35:44 +0700

    Update get cookie validate

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,14 @@
-import { refreshToken } from "@/shared/utils/fetchApi";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 // import styles from './page.module.css'
 
 export default async function Home() {
   const cookieStore = cookies();
-  const jwt_auth = cookieStore.get("jwt_auth");
   const jwt_refresh = cookieStore.get("jwt_refresh");
 
   if (!jwt_refresh) {
     redirect("/auth/login");
   } else {
-    if (!jwt_auth) {
-      const dataRefresh = {
-        refresh_token: jwt_refresh.value,
-      };
-
-      await refreshToken(dataRefresh)
-        .then(() => {
-          redirect("/dashboard");
-        })
-        .catch((err) => {
-          console.log("error refresh: ", err);
-          redirect("/auth/login");
-        });
-    } else {
-      redirect("/dashboard");
-    }
+    redirect("/dashboard");
   }
 }
