ca265ca28233b709d8180bca31058a9d07e284cc
radenlarko <radenlarko@gmail.com>
2023-05-19 09:35:43 +0700

    Update refresh token ssr schema

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,15 +1,33 @@
 import MainLayout from "@/shared/components/menu/MainLayout";
+import { refreshToken } from "@/shared/utils/fetchApi";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import React, { PropsWithChildren } from "react";
 
-const DashboardLayout = ({ children }: PropsWithChildren) => {
+const DashboardLayout = async ({ children }: PropsWithChildren) => {
   const cookieStore = cookies();
   const jwt_auth = cookieStore.get("jwt_auth");
+  const jwt_refresh = cookieStore.get("jwt_refresh");
 
-  if (!jwt_auth) {
+  if (!jwt_refresh) {
     redirect("/auth/login");
   }
+
+  if (!jwt_auth) {
+    const dataRefresh = {
+      refresh_token: jwt_refresh.value,
+    };
+
+    await refreshToken(dataRefresh)
+      .then(() => {
+        return <MainLayout>{children}</MainLayout>;
+      })
+      .catch((err) => {
+        console.log("error refresh: ", err);
+        redirect("/auth/login");
+      });
+  }
+
   return <MainLayout>{children}</MainLayout>;
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,31 @@
+import { refreshToken } from "@/shared/utils/fetchApi";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 // import styles from './page.module.css'
 
-export default function Home() {
+export default async function Home() {
   const cookieStore = cookies();
   const jwt_auth = cookieStore.get("jwt_auth");
+  const jwt_refresh = cookieStore.get("jwt_refresh");
 
-  if (jwt_auth) {
-    redirect("/dashboard");
-  } else {
+  if (!jwt_refresh) {
     redirect("/auth/login");
+  } else {
+    if (!jwt_auth) {
+      const dataRefresh = {
+        refresh_token: jwt_refresh.value,
+      };
+
+      await refreshToken(dataRefresh)
+        .then(() => {
+          redirect("/dashboard");
+        })
+        .catch((err) => {
+          console.log("error refresh: ", err);
+          redirect("/auth/login");
+        });
+    } else {
+      redirect("/dashboard");
+    }
   }
 }
