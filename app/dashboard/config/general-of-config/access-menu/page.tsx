1d0bf8342a9ab3d9774ae0009706c6619e69ce66
radenlarko <radenlarko@gmail.com>
2023-05-25 10:19:30 +0700

    Update access menu page

--- app/dashboard/config/general-of-config/access-menu/page.tsx
+++ app/dashboard/config/general-of-config/access-menu/page.tsx
@@ -3,8 +3,6 @@ import { getMenus, getUsers } from "@/shared/utils/fetchApi";
 import React from "react";
 import AccessMenuPage from "./AccessMenuPage";
 
-export const dynamic = 'force-dynamic'
-
 const AccessMenu = async () => {
   const jwt_auth = cookies().get("jwt_auth");
   const initialUser = getUsers(jwt_auth?.value);
