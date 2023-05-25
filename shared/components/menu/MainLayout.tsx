3c2772c18ba1c767b29e02cdd721f37b1eaf240c
radenlarko <radenlarko@gmail.com>
2023-05-25 11:21:55 +0700

    Update MainLayout

--- shared/components/menu/MainLayout.tsx
+++ shared/components/menu/MainLayout.tsx
@@ -1,23 +1,77 @@
 "use client";
 import React, {
   PropsWithChildren,
+  useCallback,
   useContext,
   useLayoutEffect,
   useMemo,
 } from "react";
-import { Breadcrumb, Layout, Menu, MenuProps, theme, Typography } from "antd";
+import {
+  Avatar,
+  Breadcrumb,
+  Dropdown,
+  Image,
+  Layout,
+  Menu,
+  MenuProps,
+  theme,
+  Typography,
+} from "antd";
 import { usePathname, useRouter } from "next/navigation";
 import FooterMenu from "./FooterMenu";
 import Link from "next/link";
 import { AuthContext } from "@/shared/store/AuthContext";
 import { ItemType, MenuItemType } from "antd/es/menu/hooks/useItems";
 import { AccessMenu } from "@/shared/types/login";
+import { brightColor, labelColor } from "@/shared/utils/colors";
+import fallbackAva from "@/shared/utils/fallbackAva";
+import { FiLogOut } from "react-icons/fi";
+import { useMutation } from "@tanstack/react-query";
+import { revokeToken } from "@/shared/utils/fetchApi";
+import { ErrorResponse } from "@/shared/types/error";
+import { myError } from "@/shared/utils/myError";
+import styles from "./menu.module.css";
+
+interface UserAvaProps {
+  src: string;
+  size: number;
+}
 
 const { Header, Content } = Layout;
 const { Text } = Typography;
 
+const UserAva = ({ src, size }: UserAvaProps) => {
+  return (
+    <Avatar
+      style={{
+        backgroundColor: brightColor,
+      }}
+      size={size}
+      src={
+        <Image
+          src={src}
+          style={{
+            width: size,
+          }}
+          preview={false}
+          fallback={fallbackAva}
+          alt="user-profile"
+        />
+      }
+    />
+  );
+};
+
 const MainLayout = ({ children }: PropsWithChildren) => {
-  const { isLoading, userMenu, setMenuAction } = useContext(AuthContext);
+  const {
+    isLoading,
+    userData,
+    userMenu,
+    userRefresh,
+    signOut,
+    setMenuAction,
+    handleRefreshToken,
+  } = useContext(AuthContext);
   const { push } = useRouter();
   const pathname = usePathname();
   const {
@@ -61,6 +115,50 @@ const MainLayout = ({ children }: PropsWithChildren) => {
     return recursiveMenu([menuDashboard, ...userMenu]);
   }, [userMenu]);
 
+  const itemsProfile = useMemo<ItemType<MenuItemType>[]>(() => {
+    return [
+      {
+        label: (
+          <div>
+            <UserAva src={`${userData.profile.avaImage}`} size={48} />
+            <div style={{ marginTop: 6, fontSize: 16, width: 110 }}>
+              <Text type="secondary" strong ellipsis>
+                {userData.profile.name}
+              </Text>
+            </div>
+          </div>
+        ),
+        key: "avatar",
+      },
+      {
+        label: "Logout",
+        key: "logout",
+        icon: <FiLogOut />,
+        danger: true,
+      },
+    ];
+  }, [userData.profile]);
+
+  const { mutate } = useMutation(revokeToken, {
+    onSuccess: (dataSuccess) => {
+      console.log("data success: ", dataSuccess);
+
+      signOut();
+    },
+    onError: (err: ErrorResponse) => myError(err, handleRefreshToken),
+  });
+
+  const handleSignOut = useCallback(() => {
+    mutate({ refresh_token: userRefresh });
+  }, [mutate, userRefresh]);
+
+  const onClickMenuProfile: MenuProps["onClick"] = (e) => {
+    console.log("click Profile ", e);
+    if (e.key === "logout") {
+      handleSignOut();
+    }
+  };
+
   const onClick: MenuProps["onClick"] = (e) => {
     push(`/dashboard${e.key}`);
   };
@@ -96,9 +194,28 @@ const MainLayout = ({ children }: PropsWithChildren) => {
           mode="horizontal"
           defaultSelectedKeys={["/dashboard"]}
           items={menuList}
-          style={{ width: "100%" }}
+          style={{ flex: 1, margin: "0 10px" }}
           onClick={onClick}
         />
+        <Dropdown
+          menu={{
+            style: { padding: 10, borderRadius: "15px" },
+            onClick: onClickMenuProfile,
+            items: itemsProfile,
+          }}
+        >
+          <div className={styles.navbarAvatarContainer}>
+            <UserAva src={`${userData.profile.avaImage}`} size={36} />
+            <div className={styles.navbarWelcome}>
+              <Text style={{ color: labelColor, lineHeight: 1, fontSize: 12 }}>
+                Welcome
+              </Text>
+              <Text strong style={{ color: "white", lineHeight: 1.5 }}>
+                {userData.profile.name}
+              </Text>
+            </div>
+          </div>
+        </Dropdown>
       </Header>
       <Content style={{ padding: "0 50px" }}>
         <Breadcrumb
---
+++ shared/utils/fallbackAva.ts
@@ -0,0 +1,4 @@
+const fallbackAva =
+  "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80";
+
+export default fallbackAva;
